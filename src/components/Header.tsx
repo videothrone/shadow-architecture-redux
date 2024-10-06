d5c6c89bc2ce3eb7d6b1a7741dd30c11ab1a6418
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-10-06 17:04:06 +0200

    ✨ Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <h1>
+          S h a d o w <span className="header__slash">/</span> A r c h i t e c t
+          u r e
+        </h1>
+      </div>
+    </>
+  );
+}
