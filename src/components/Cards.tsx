ef7a7dceb67c457ecd573d663278fb7e525fa507
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-10-16 18:44:59 +0200

    ✨ Add Overlay functionality to Card component

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import placesData from "../assets/data/markers.json";
 import Card from "./Card";
+import Overlay from "./Overlay";
+import { Place } from "../types/types";
 
 export default function Cards() {
+  const [selectedPlace, setSelectedPlace] = useState<Place | null>(null);
+  const [overlayIsVisible, setOverlayIsVisible] = useState(false);
+
+  const handleCardClick = (place: Place) => {
+    setSelectedPlace(place);
+    setOverlayIsVisible(true);
+  };
+
   return (
-    <div className="cards">
-      {placesData.places.map((place, index) => (
-        <Card key={index} place={place} index={index} />
-      ))}
-    </div>
+    <>
+      <div className="cards">
+        {placesData.places.map((place, index) => (
+          <Card
+            key={index}
+            place={place}
+            index={index}
+            onClick={() => handleCardClick(place)}
+          />
+        ))}
+      </div>
+      {selectedPlace && (
+        <Overlay
+          selectedPlace={selectedPlace}
+          setOverlayIsVisible={setOverlayIsVisible}
+          overlayIsVisible={overlayIsVisible}
+        />
+      )}
+    </>
   );
 }
--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Place } from "../types/types";
 
 type Props = {
@@ -13,12 +13,31 @@ export default function Overlay({
 }: Props) {
   const [extendedImage, setExtendedImage] = useState<string | null>(null);
 
+  useEffect(() => {
+    const handleKeydown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setOverlayIsVisible(false);
+      }
+    };
+
+    if (overlayIsVisible) {
+      window.addEventListener("keydown", handleKeydown);
+    }
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  }, [overlayIsVisible, setOverlayIsVisible]);
+
   return (
     <>
       {overlayIsVisible && (
         <div id="marker-overlay" className="disable-scrollbars">
           <div id="marker-overlay-content-container">
-            <div id="x" onClick={() => setOverlayIsVisible(false)}>
+            <div
+              className="close-button"
+              onClick={() => setOverlayIsVisible(false)}
+            >
               X
             </div>
             <h2>{selectedPlace.properties.NAME}</h2>
