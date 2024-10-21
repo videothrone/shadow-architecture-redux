80c9ff0fda367f5de72ce028b441b274fbb6be18
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-10-21 18:21:00 +0200

    💄 Change flexed Cards to grid layout

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,9 +8,13 @@ type Props = {
 
 export default function Card({ place, index, onClick }: Props) {
   return (
-    <div key={index} className="card card__animation" onClick={onClick}>
+    <div key={index} className="card card--animation" onClick={onClick}>
       <h2 className="card__headline">{place.properties.SHORT_NAME}</h2>
-      <img src={place.properties.IMAGEURL} className="card__image" />
+      <img
+        src={place.properties.IMAGEURL}
+        className="card__image"
+        alt={`Picture of ${place.properties.NAME}`}
+      />
       <div className="card__description">{place.properties.DESCRIPTION}</div>
     </div>
   );
