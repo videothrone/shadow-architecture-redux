1270312d796bd2ed39637bab2a52c728bca196b9
Ferenc <47146569+videothrone@users.noreply.github.com>
2024-10-24 19:15:42 +0200

    ♻️ Clean up Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,10 @@
 export default function Header() {
   return (
-    <>
-      <div className="header">
-        <h1>
-          S h a d o w <span className="header__slash">/</span> A r c h i t e c t
-          u r e
-        </h1>
-      </div>
-    </>
+    <div className="header">
+      <h1 className="header__headline h1">
+        S h a d o w <span className="header__headline-slash">/</span> A r c h i
+        t e c t u r e
+      </h1>
+    </div>
   );
 }
